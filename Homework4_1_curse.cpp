54a89c30f6ece84574a8fa525f42de59df0cfab5
lilia <lilia@DESKTOP-64J80RS>
2024-11-10 16:42:28 +0200

    Добавьте файлы проекта.

---
+++ Homework4_1_curse.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a = 9;
+    int b = 11;
+
+
+    if (a > b) {
+        a = b;
+    }
+    else {
+        b = a;
+    }
+
+   
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+
+    return 0;
+    
+}
+
